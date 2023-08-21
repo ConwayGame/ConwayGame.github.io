0687b15deba573a7081218741b9630dd43ea93b7
Yurii Antonchyk <xsat.php@gmail.com>
2023-08-21 20:51:42 +0300

    Moved build directory to root

--- README.md
+++
@@ -1,1 +0,0 @@
-# ConwayGame.github.io
--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask
 from flask_frozen import Freezer
+from os import getcwd
+
 
 app = Flask(__name__)
 freezer = Freezer(app)
 
+app.config['FREEZER_DESTINATION'] = getcwd()
+app.config['FREEZER_DESTINATION_IGNORE'] = '*'
+
 
 @app.route("/")
 def hello():
-    return "Test flask on github pages"
+    return "Re:Test flask on github pages"
 
 
 if __name__ == "__main__":
     freezer.freeze()
-    app.run(port=8000)
+    # app.run(port=8000)
